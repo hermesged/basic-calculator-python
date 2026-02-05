43b24cd8e52f9f9596a20e898b005d9059bf892f
Hermes Gédéon HOUNKANLIN <hermesged@gmail.com>
2026-02-05 12:53:49 +0100

    feat: use of dictionnary and anonymous function: lambda

--- calculator.py
+++ calculator.py
@@ -1,21 +1,19 @@
 def calculator():
+    
+    operations = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y if y != 0 else "Error: Division by zero is not allowed."
+    }
+    
     print('Welcome to the calculator programme!')
     nombre1 = float(input('Please enter the first number: '))    
     nombre2 = float(input('Please enter the second number: '))
     operation = input('Please enter the operation you want to perform (+, -, *, /): ')
     
-    if operation == '+':
-        result = nombre1 + nombre2
-    elif operation == '-':
-        result = nombre1 - nombre2
-    elif operation == "*":
-        result = nombre1 * nombre2
-    elif operation == "/":
-        if nombre2 != 0:
-            result = nombre1 / nombre2
-        else:
-            print("Error: Division by zero is not allowed.")
-            return
+    if operation in operations:
+        result = operations[operation](nombre1, nombre2)
     else:
         print("Error: Invalid operation.")
         return
